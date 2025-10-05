dae23e89331a39534f70f1e08a334c7dbbe51388
dediprasetya <129157815+dediprasetya@users.noreply.github.com>
2025-10-05 18:24:45 +0700

    update skrip arduino

--- sketch_oct1a_smartbin2_hilirasai.ino
+++ sketch_oct1a_smartbin2_hilirasai.ino
@@ -1,5 +1,4 @@
 #include <WiFi.h>
-#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include <LiquidCrystal_I2C.h>
@@ -8,7 +7,7 @@
 const char* ssid = "vivo 1938";
 const char* password = "dediprasetya";
 
-// MQTT server
+// MQTT broker
 const char* mqtt_server = "hilirisasi.revolusi-it.com";
 const int mqtt_port = 1883;
 const char* mqtt_user = "hilirisasi";
@@ -20,14 +19,10 @@ const char* mqtt_pass = "penelitianhilirisasi25";
 #define trigB 25
 #define echoB 33
 
-WiFiClientSecure secureClient;
-PubSubClient client(secureClient);
-
-// LCD
+WiFiClient espClient;
+PubSubClient client(espClient);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-// ID tong (ubah sesuai kebutuhan)
-const int idTong = 1;  
+const int bin_id = 1;
 
 void setup_wifi() {
   WiFi.begin(ssid, password);
@@ -36,18 +31,21 @@ void setup_wifi() {
     delay(500);
     Serial.print(".");
   }
-  Serial.println(" connected!");
+  Serial.println("\nWiFi connected");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 void reconnect() {
   while (!client.connected()) {
-    Serial.print("Menghubungkan ke MQTT...");
-    if (client.connect("ESP32_bin1", mqtt_user, mqtt_pass)) {
-      Serial.println("terhubung");
+    Serial.print("Connecting to MQTT...");
+    String clientId = "ESP32Client-" + String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str(), mqtt_user, mqtt_pass)) {
+      Serial.println("connected");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
-      Serial.println(" mencoba dalam 5 detik");
+      Serial.println(" retrying in 5 seconds");
       delay(5000);
     }
   }
@@ -64,7 +62,6 @@ long medianDistance(int trigPin, int echoPin) {
     readings[i] = pulseIn(echoPin, HIGH, 30000) * 0.034 / 2;
     delay(50);
   }
-  // ambil median dari 3 data
   for (int i = 0; i < 2; i++) {
     for (int j = i + 1; j < 3; j++) {
       if (readings[i] > readings[j]) {
@@ -86,43 +83,36 @@ void setup() {
 
   lcd.init();
   lcd.backlight();
-  lcd.print("Menghubungkan...");
+  lcd.print("Connecting...");
 
   setup_wifi();
-  secureClient.setInsecure();
   client.setServer(mqtt_server, mqtt_port);
   reconnect();
 
   lcd.clear();
-  lcd.print("Tong Ready");
+  lcd.print("Bin Ready");
 }
 
 void loop() {
   if (!client.connected()) reconnect();
   client.loop();
 
-  long distance1 = medianDistance(trigA, echoA);
-  long distance2 = medianDistance(trigB, echoB);
+  long jarakA = medianDistance(trigA, echoA);
+  long jarakB = medianDistance(trigB, echoB);
 
-  // Buat JSON { "id":1, "d1":..., "d2":... }
   StaticJsonDocument<200> doc;
-  doc["id"] = idTong;
-  doc["d1"] = distance1;
-  doc["d2"] = distance2;
+  doc["id"] = bin_id;
+  doc["d1"] = jarakA;
+  doc["d2"] = jarakB;
 
   char buffer[200];
   size_t n = serializeJson(doc, buffer);
   client.publish("tong/sensor", buffer, n);
 
-  // Tampilkan di LCD
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Sensor A:" + String(distance1) + "cm");
-  lcd.setCursor(0, 1);
-  lcd.print("Sensor B:" + String(distance2) + "cm");
-
-  // Tampilkan juga di Serial Monitor
-  Serial.print("JSON sent: ");
+  lcd.print("A:" + String(jarakA) + " B:" + String(jarakB));
+  Serial.print("Sent: ");
   Serial.println(buffer);
 
   delay(5000);
